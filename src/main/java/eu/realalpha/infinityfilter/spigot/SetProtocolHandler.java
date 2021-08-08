4e995e7c5fc84293051308da89322c4b33ce8374
Maxlego08 <maxlego08@sfr.fr>
2021-08-08 21:34:06 +0200

    :fire: Remove unuse code

--- src/main/java/eu/realalpha/infinityfilter/spigot/SetProtocolHandler.java
+++ src/main/java/eu/realalpha/infinityfilter/spigot/SetProtocolHandler.java
@@ -14,47 +14,45 @@ import java.util.logging.Level;
 
 public class SetProtocolHandler extends PacketAdapter {
 
-    private final FilterSpigot filterSpigot;
+	private final FilterSpigot filterSpigot;
 
-    public SetProtocolHandler(FilterSpigot filterSpigot) {
-        super(filterSpigot, ListenerPriority.NORMAL, PacketType.Handshake.Client.SET_PROTOCOL);
-        this.filterSpigot = filterSpigot;
-    }
+	public SetProtocolHandler(FilterSpigot filterSpigot) {
+		super(filterSpigot, ListenerPriority.NORMAL, PacketType.Handshake.Client.SET_PROTOCOL);
+		this.filterSpigot = filterSpigot;
+	}
 
-    private static Class<?> abstractChannelClass;
+	static {
+		try {
+			Class.forName("io.netty.channel.AbstractChannel");
+		} catch (ClassNotFoundException e) {
+			try {
+				Class.forName("net.minecraft.util.io.netty.channel.AbstractChannel");
+			} catch (ClassNotFoundException e2) {
+				throw new RuntimeException(new ReflectionException(e2));
+			}
 
-    static {
-        try {
-            abstractChannelClass = Class.forName("io.netty.channel.AbstractChannel");
-        } catch (ClassNotFoundException e) {
-            try {
-                abstractChannelClass = Class.forName("net.minecraft.util.io.netty.channel.AbstractChannel");
-            } catch (ClassNotFoundException e2) {
-                throw new RuntimeException(new ReflectionException(e2));
-            }
+		}
+	}
 
-        }
-    }
+	@Override
+	public void onPacketReceiving(PacketEvent event) {
+		PacketContainer packet = event.getPacket();
+		Player player = event.getPlayer();
+		if (packet.getProtocols().read(0) == PacketType.Protocol.LOGIN) {
+			try {
+				String hostname = packet.getStrings().read(0);
+				boolean hasToken = hostname.contains(filterSpigot.getKey());
+				ForwardContext forwardContext = (hasToken ? ForwardContext.of(hostname) : ForwardContext.empty());
+				Forward forward = new SpigotForward(player);
+				if (hasToken) {
+					forward.setAddress(forwardContext.getInetSocketAddress());
+					packet.getStrings().write(0, forwardContext.getHost());
+				} else if (filterSpigot.isOnlineMode())
+					forward.disconnect();
+			} catch (Exception e) {
+				this.getPlugin().getLogger().log(Level.SEVERE, e, () -> "");
+			}
+		}
 
-
-    @Override
-    public void onPacketReceiving(PacketEvent event) {
-        PacketContainer packet = event.getPacket();
-        Player player = event.getPlayer();
-        if (packet.getProtocols().read(0) == PacketType.Protocol.LOGIN) {
-            try {
-                String hostname = packet.getStrings().read(0);
-                boolean hasToken = hostname.contains(filterSpigot.getKey());
-                ForwardContext forwardContext = (hasToken ? ForwardContext.of(hostname) : ForwardContext.empty());
-                Forward forward = new SpigotForward(player);
-                if (hasToken){
-                    forward.setAddress(forwardContext.getInetSocketAddress());
-                    packet.getStrings().write(0, forwardContext.getHost());
-                }else if(filterSpigot.isOnlineMode()) forward.disconnect();
-            } catch (Exception e) {
-                this.getPlugin().getLogger().log(Level.SEVERE, e, () -> "");
-            }
-        }
-
-    }
+	}
 }
